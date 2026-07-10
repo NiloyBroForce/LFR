3ca83c5e1fd091bdbd74f94818c6f00439634d00
Niloy <niloy.rolling@gmail.com>
2026-07-10 21:14:37 +0600

    Create follower.ino

---
+++ follower.ino
@@ -0,0 +1,364 @@
+// #include <NewPing.h>
+//
+// NewPing sonarfront(6, 6, 20);
+//
+// int mid_distance = 10;
+#include<Arduino.h>
+#define lmf 4
+#define lmb 5
+#define lms 3
+#define rmf 9
+#define rmb 8
+#define rms 10
+#define sw 12
+#define head_light 11
+
+#define spl 14
+#define spr 10
+#define dr 3
+#define tsp 80
+#define tbr 40
+#define side 2
+#define node_delay 15
+#define stop_timer 40
+#define u_turn_timer 100
+#define return_delay 450
+#define highest_brake 0
+#define lowest_brake 0
+#define brake_constant 1
+#define br_delay 50
+
+// Function declarations
+void setup();
+void loop();
+
+void motor(int a, int b);
+void braking();
+
+void reading();
+
+void show_digital();
+void show_analog();
+
+void line_follow();
+
+int strt = lowest_brake * brake_constant;
+int peak = highest_brake * brake_constant;
+int cl = strt;
+
+int pos = 0;
+char flag = 's';
+uint32_t m1, m2, m3, s1;
+
+int s[6], sensor, sum = 0, distance;
+int base[6] = {1, 2, 4, 8, 16, 32};
+int mid[6] = {300, 300, 300, 300, 300, 300};
+int s_digital[6];
+
+byte counter = 0;
+byte cross = 0;
+
+void setup()
+{
+    pinMode(lmf, OUTPUT);
+    pinMode(lmb, OUTPUT);
+    pinMode(rmf, OUTPUT);
+    pinMode(rmb, OUTPUT);
+    pinMode(head_light, OUTPUT);
+    pinMode(sw, INPUT_PULLUP);
+    Serial.begin(9600);
+}
+
+void loop()
+{
+
+    //  show_analog();  // Or show_digital(
+    //   delay(200);     // Gives the Serial buffer time to breathe
+    line_follow();
+    // motor(10 * spl,10 * spr);
+}
+
+void motor(int a, int b)
+{
+    if (a > 0)
+    {
+        digitalWrite(lmf, 1);
+        digitalWrite(lmb, 0);
+    }
+    else if (a < 0)
+    {
+        a = -a;
+        digitalWrite(lmf, 0);
+        digitalWrite(lmb, 1);
+    }
+    else
+    {
+        digitalWrite(lmf, 0);
+        digitalWrite(lmb, 0);
+    }
+
+    if (b > 0)
+    {
+        digitalWrite(rmf, 1);
+        digitalWrite(rmb, 0);
+    }
+    else if (b < 0)
+    {
+        b = -b;
+        digitalWrite(rmf, 0);
+        digitalWrite(rmb, 1);
+    }
+    else
+    {
+        digitalWrite(rmf, 0);
+        digitalWrite(rmb, 0);
+    }
+    analogWrite(lms, a);
+    analogWrite(rms, b);
+}
+
+void braking()
+{
+    motor(-200, -200);
+    delay(cl / brake_constant);
+    motor(0, 0);
+    delay(br_delay);
+    cl = strt;
+}
+// Go to your main tab and make sure you declare both arrays:
+// int s[6];         // Holds raw analog values (0-1023)
+// int s_digital[6]; // Holds digital values (0 or 1)
+
+void reading()
+{
+    sensor = 0;
+    sum = 0;
+    for (int i = 0; i <= 5; i++)
+    {
+        s[i] = analogRead(i);
+
+        // Save the digital 1 or 0 to a SEPARATE array
+        s_digital[i] = (s[i] > mid[i]) ? 1 : 0;
+
+        sensor += s_digital[i] * base[i];
+        sum += s_digital[i];
+    }
+}
+
+void show_digital()
+{
+    reading(); // This updates both s[] and s_digital[] safely
+    for (int i = 5; i >= 0; i--)
+    {
+        Serial.print(s_digital[i]); // Print from the digital array
+        Serial.print(" ");
+    }
+    Serial.println(sensor, BIN);
+}
+
+void show_analog()
+{
+    reading(); // Just call reading to refresh the data safely
+    for (int i = 5; i >= 0; i--)
+    {
+        Serial.print(s[i]); // Print the raw analog data
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+void line_follow()
+{
+    while (digitalRead(sw))
+    { // infinite loop
+    a:
+        reading();
+        if (sum == 0)
+        {
+            if (flag != 's')
+            {
+                delay(node_delay);
+                braking();
+                (flag == 'l') ? motor(-tsp, tsp - 30) : motor(tsp, -tsp);
+                while (s[2] == 0 && s[3] == 0)
+                    reading();
+                (flag == 'l') ? motor(tsp, -tsp) : motor(-tsp, tsp);
+                delay(tbr);
+                pos = 0;
+                flag = 's';
+                cross = 0; // when you are done turning robot, make the flag to its normal state so that robot wont turn on its own when it finds no line without detecting 90degree logic
+            }
+
+            else if (pos < 4 && pos > -4)
+            {
+                m3 = millis();
+                while (sum == 0)
+                {
+                    reading();
+                    m2 = millis();
+                    if (m2 - m3 > u_turn_timer)
+                    {
+                        braking();
+                        digitalWrite(head_light, HIGH);
+                        (side == 1) ? motor(-tsp, tsp) : motor(tsp, -tsp);
+                        while (s[2] == 0 && s[3] == 0)
+                            reading();
+                        (side == 1) ? motor(tsp, -tsp) : motor(-tsp, tsp);
+                        delay(tbr);
+                        pos = 0;
+                        digitalWrite(head_light, LOW);
+                        break;
+                    }
+                }
+            }
+        }
+        else if (sum == 1 || sum == 2)
+        { // only for straight line
+            if (cross != 0)
+            {
+                braking();
+                (cross == 1) ? motor(-tsp, tsp) : motor(tsp, -tsp);
+                while (sum > 0)
+                    reading();
+                while (s[2] == 0 && s[3] == 0)
+                    reading();
+                (cross == 1) ? motor(tsp, -tsp) : motor(-tsp, tsp);
+                delay(tbr);
+                pos = 0;
+                flag = 's';
+                cross = 0;
+            }
+
+            else if (sensor == 0b001100)
+            {
+                if (pos != 0)
+                {
+                    (pos > 0) ? motor(-10 * spl, 10 * spr) : motor(10 * spl, -10 * spr);
+                    (pos > 0) ? delay(pos * dr) : delay(-pos * dr);
+                    pos = 0; // this logic is for those whose bot is wabbling during high speed run. this logic will forcefully return bot to its midpoint. don't give more than 30ms delay here!
+                }
+                motor(10 * spl, 10 * spr);
+                if (cl < peak)
+                    cl++;
+            }
+            // right side portion
+            else if (sensor == 0b000100)
+            {
+                motor(10 * spl, 8 * spr);
+                if (cl < peak)
+                    cl++;
+            }
+            else if (sensor == 0b000110)
+            {
+                if (pos < 1)
+                    pos = 1;
+                motor(10 * spl, 8 * spr);
+            }
+            else if (sensor == 0b000010)
+            {
+                if (pos < 2)
+                    pos = 2;
+                motor(10 * spl, -1 * spr);
+            }
+            else if (sensor == 0b000011)
+            {
+                if (pos < 3)
+                    pos = 3;
+                motor(10 * spl, 6 * spr);
+                flag = 'r';
+            }
+            else if (sensor == 0b000001)
+            {
+                if (pos < 4)
+                    pos = 4;
+                motor(10 * spl, 3 * spr);
+                if (cl > strt)
+                    cl--;
+                flag = 'r';
+            }
+            // left side portion
+            else if (sensor == 0b001000)
+            {
+                motor(8 * spl, 10 * spr);
+                if (cl < peak)
+                    cl++;
+            }
+            else if (sensor == 0b011000)
+            {
+                if (pos > -1)
+                    pos = -1;
+                motor(8 * spl, 10 * spr);
+            }
+            else if (sensor == 0b010000)
+            {
+                if (pos > -2)
+                    pos = -2;
+                motor(-1 * spl, 10 * spr);
+            }
+            else if (sensor == 0b110000)
+            {
+                if (pos > -3)
+                    pos = -3;
+                motor(5 * spl, 10 * spr);
+                flag = 'l';
+            }
+            else if (sensor == 0b100000)
+            {
+                if (pos > -4)
+                    pos = -4;
+                motor(2 * spl, 10 * spr);
+                if (cl > strt)
+                    cl--;
+                flag = 'l';
+            }
+        }
+
+        else if (sum == 3 || sum == 4 || sum == 5)
+        { // only for sharp turn logic
+            motor(10 * spl, 10 * spr);
+            if (s[0] == 0 && s[5] == 1 && s[2] + s[3] + s[4] > 0)
+            {
+
+                flag = 'l';
+            }
+            else if (s[0] == 1 && s[5] == 0 && s[1] + s[2] + s[3] > 0)
+            {
+                flag = 'r';
+            }
+            m1 = millis();
+        }
+
+        else if (sum == 6)
+        {
+            m3 = millis();
+            flag = 's';
+            cross = 2;
+            pos = 0; // turn no 1
+            while (s[0] == 1 || s[5] == 1)
+            {
+                reading();
+                m2 = millis();
+                if (m2 - m3 > stop_timer)
+                {
+                    motor(-250, -250);
+                    delay(cl / brake_constant);
+                    motor(0, 0);
+                    digitalWrite(head_light, HIGH);
+                    while (sum == 6 && digitalRead(sw))
+                        reading();
+                    digitalWrite(head_light, LOW);
+                    if (digitalRead(sw))
+                    {
+                        delay(1000);
+                        goto a;
+                    }
+                    break;
+                }
+            }
+            m1 = millis();
+        }
+        m2 = millis();
+        if (m2 - m1 > 200)
+            flag = 's';
+    }
+}
